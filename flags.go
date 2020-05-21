e922b154ced03ec749ee0bd4fa8d1c92266c63b5
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-05-21 03:45:31 -0400

    Initial poc structure, from chat w/rendevouz example

---
+++ flags.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"strings"
+
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	maddr "github.com/multiformats/go-multiaddr"
+)
+
+// A new type we need for writing a custom flag parser
+type addrList []maddr.Multiaddr
+
+func (al *addrList) String() string {
+	strs := make([]string, len(*al))
+	for i, addr := range *al {
+		strs[i] = addr.String()
+	}
+	return strings.Join(strs, ",")
+}
+
+func (al *addrList) Set(value string) error {
+	addr, err := maddr.NewMultiaddr(value)
+	if err != nil {
+		return err
+	}
+	*al = append(*al, addr)
+	return nil
+}
+
+func StringsToAddrs(addrStrings []string) (maddrs []maddr.Multiaddr, err error) {
+	for _, addrString := range addrStrings {
+		addr, err := maddr.NewMultiaddr(addrString)
+		if err != nil {
+			return maddrs, err
+		}
+		maddrs = append(maddrs, addr)
+	}
+	return
+}
+
+type keystoreFile struct {
+	PrivateKey string `json:"privateKey"`
+	ID         string `json:"ID"`
+}
+
+type Config struct {
+	RendezvousString string
+	BootstrapPeers   addrList
+	ListenAddresses  addrList
+	ProtocolID       string
+	KeystorePath     string
+	PeersPath        string
+}
+
+func ParseFlags() (Config, error) {
+	config := Config{}
+	flag.StringVar(&config.RendezvousString, "rendezvous", "secret-engine-rendezvous",
+		"Unique string to identify group of nodes. Share this with your friends to let them connect with you")
+	flag.Var(&config.BootstrapPeers, "peer", "Adds a peer multiaddress to the bootstrap list")
+	flag.Var(&config.ListenAddresses, "listen", "Adds a multiaddress to the listen list")
+	flag.StringVar(&config.ProtocolID, "pid", "/secrets/0.1.0", "Sets a protocol id for stream headers")
+	flag.StringVar(&config.KeystorePath, "keystore", "./ks0.json", "Specify a keystore file")
+	flag.StringVar(&config.PeersPath, "peers", "./peers.json", "Peers to create a Secret Ring with"
+	flag.Parse()
+
+	if len(config.BootstrapPeers) == 0 {
+		config.BootstrapPeers = dht.DefaultBootstrapPeers
+	}
+
+	return config, nil
+}
