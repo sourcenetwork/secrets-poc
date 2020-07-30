62c61501e8c4220e7b5e51de7d53bfd824820deb
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-07-30 10:45:41 -0400

    more edwards25519 experiments

--- keygen/main.go
+++ keygen/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 
 	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/peer"
+
+	"go.dedis.ch/kyber/v3"
+	"go.dedis.ch/kyber/v3/group/edwards25519"
 )
 
 type keystoreFile struct {
@@ -14,32 +16,93 @@ type keystoreFile struct {
 	ID         string `json:"ID"`
 }
 
+func publicKeyToEdwards25519KyberPoint(pubkey []byte) kyber.Point {
+	suite := edwards25519.NewBlakeSHA256Ed25519()
+	point := suite.Point()
+	point.UnmarshalBinary(pubkey)
+	return point
+}
+
+func privateKeyToEdwards25519KyberPoint(priv []byte) kyber.Scalar {
+	suite := edwards25519.NewBlakeSHA256Ed25519()
+	scalar := suite.Scalar()
+	scalar.UnmarshalBinary(priv)
+	return scalar
+}
+
 func main() {
-	priv, _, err := crypto.GenerateKeyPair(
-		crypto.Ed25519, // Select your key type. Ed25519 are nice short
-		-1,             // Select key length when possible (i.e. RSA).
-	)
+	suite := edwards25519.NewBlakeSHA256Ed25519()
+	rand := suite.RandomStream()
+	privTemp := suite.Scalar().Pick(rand)       // create a private key x
+	pubTemp := suite.Point().Mul(privTemp, nil) // public key
+	b, _ := privTemp.MarshalBinary()
+	buf := bytes.NewBuffer(b)
+	priv, pub, err := crypto.GenerateEd25519Key(buf)
 	if err != nil {
 		panic(err)
 	}
 
-	id, err := peer.IDFromPrivateKey(priv)
+	fmt.Println("Private:")
+	privCBytes, err := priv.Raw()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("P2P:", hex.EncodeToString(privCBytes))
 
-	privBytes, err := priv.Bytes()
+	privKBytes, err := privTemp.MarshalBinary()
 	if err != nil {
 		panic(err)
 	}
-	privStr := hex.EncodeToString(privBytes)
+	fmt.Println("Kyber:", hex.EncodeToString(privKBytes))
 
-	keystore := keystoreFile{PrivateKey: privStr, ID: id.String()}
-
-	buf, err := json.Marshal(keystore)
+	fmt.Println("\nPublic:")
+	pubCBytes, err := pub.Raw()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("P2P:", hex.EncodeToString(pubCBytes))
 
-	fmt.Println(string(buf))
+	pubKBytes, err := pubTemp.MarshalBinary()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Kyber1:", pubTemp.String())
+	fmt.Println("Kyber2:", hex.EncodeToString(pubKBytes))
+
+	// pubBytes1, _ := pub.Raw()
+	// fmt.Println(publicKeyToEdwards25519KyberPoint(pubBytes1).String())
+	// fmt.Println(pubTemp.String())
+	// fmt.Println("--")
+
+	// // priv, _, err := crypto.GenerateKeyPair(
+	// // 	crypto.Ed25519, // Select your key type. Ed25519 are nice short
+	// // 	-1,             // Select key length when possible (i.e. RSA).
+	// // )
+	// // if err != nil {
+	// // 	panic(err)
+	// // }
+
+	// id1, _ := peer.IDFromPrivateKey(priv)
+
+	// fmt.Println("Kyber public:", pubTemp.String())
+	// pubL, _ := id1.ExtractPublicKey()
+	// pubBytes, _ := pubL.Bytes()
+	// fmt.Println(hex.EncodeToString(pubBytes))
+	// point := publicKeyToEdwards25519KyberPoint(pubBytes)
+	// fmt.Println(point.String())
+
+	// privBytes, err := priv.Bytes()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// privStr := hex.EncodeToString(privBytes)
+
+	// keystore := keystoreFile{PrivateKey: privStr, ID: id1.String()}
+
+	// out, err := json.Marshal(keystore)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(string(out))
 }
